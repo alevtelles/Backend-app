693756f32c758d1712cfbf73fbdc56752d752a39
Valente Alexsander <alevtelles@gmail.com>
2025-03-24 10:45:39 -0300

    add task enum, task model e uuid

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -3,3 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 export function genrateInviteCode() {
     return uuidv4().replace(/-/g, "").slice(0, 8);
 }
+
+export function generateTaskCode() {
+    return `task-${uuidv4().replace(/-/g, "").substring(0, 3)}`;
+}
